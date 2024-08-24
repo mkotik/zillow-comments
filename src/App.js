ed44af8c446e9d4426d367d6bdd057264c92c842
Marat Kotik <maratkotik97@gmail.com>
2024-08-24 13:28:53 -0400

    add base page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import CommentPage from "./CommentPage";
+import BasePage from "./BasePage";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/homedetails/*" element={<CommentPage />} />
-          {/* Add other routes here if needed */}
+          <Route path="/*" element={<BasePage />} />
         </Routes>
       </div>
     </Router>
---
+++ src/BasePage.js
@@ -0,0 +1,5 @@
+const BasePage = () => {
+  return <div>Welcome to the Zillow Comment Page</div>;
+};
+
+export default BasePage;
