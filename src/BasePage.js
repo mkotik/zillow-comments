0802d5915798045add8c5c84c4ed87c71a773f5e
Marat Kotik <maratkotik97@gmail.com>
2024-08-24 21:39:51 -0400

    add route

--- src/BasePage.js
+++ src/BasePage.js
@@ -1,5 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+
 const BasePage = () => {
-  return <div>Welcome to the Zillow Comment Page</div>;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(
+      "/homedetails/1155-Sandpiper-St-G6-Naples-FL-34112/54181396_zpid/"
+    );
+  };
+  return (
+    <div>
+      <h1>Welcome to the Zillow Comment Page</h1>
+      <Button variant="contained" color="primary" onClick={handleNavigate}>
+        Go to Comments
+      </Button>
+    </div>
+  );
 };
 
 export default BasePage;
